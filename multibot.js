bca9b5dd36865bff614dd0bb5d2c527a2752fb0b
DatScribble <33287563+DatScribble@users.noreply.github.com>
2018-10-22 20:53:01 -0400

    Update multibot.js

--- multibot.js
+++ multibot.js
@@ -3,11 +3,12 @@ const client = new Discord.Client();
 
 client.on('ready', {} => {
   console.log('I am ready!');
+});
   
- client.on('message', message =>{
+client.on('message', message =>{
   if (message.content === 'ping') {
     message.reply('pong');
-    }
-  });
+  }
+});
   
-  client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
